935e2937067d952eed2823365f642f94429ca1b4
WIN 10 <WIN 10@Sumeet>
2019-04-30 21:14:20 +0530

    calculator

--- src/main/java/main/Calculator.java
+++ src/main/java/main/Calculator.java
@@ -5,7 +5,7 @@ public class Calculator {
     public static void main(String[] args)
     {
         System.out.println("Enter first and second number:");
-        Scanner inp= new Scanner(System.in);
+        Scanner inp = new Scanner(System.in);
         int num1,num2;
         num1 = inp.nextInt();
         num2 = inp.nextInt();
@@ -13,51 +13,54 @@ public class Calculator {
         System.out.println("Enter your selection: 1 for Addition, 2 for substraction 3 for Multiplication and 4 for division:");
         int choose;
         choose = inp.nextInt();
-        switch (choose){
-        case 1:
+        if(choose == 1)
+        {
             System.out.println(add( num1,num2));
-            break;
-        case 2:
+        }
+        else if(choose == 2)
+        {
             System.out.println(sub( num1,num2));
-            break;      
-        case 3:
+        }
+        else if(choose == 3)
+        {
             System.out.println(mult( num1,num2));
-            break;
-        case 4:
+        }
+        else if(choose == 4)
+        {
             System.out.println(div( num1,num2));
-            break;
-            default:
+        }
+        else
+        {
                 System.out.println("Illigal Operation");
+        }
 
 
         }
 
 
 
-    }
     public static int add(int x, int y)
     {
     	int result=0;
-//        write your code here 
-    	
+        result = x + y;    	
         return result;
     }
     public static int sub(int x, int y)
     {
     	int result=0;
-//        write your code here
-        return result;
+    	result = x - y;
+    	return result;
     }
     public static int mult(int x, int y)
     {
     	int result=0;
-//      write your code here        
+    	result = x * y;       
     	return result;
     }
     public static int div(int x, int y)
     {
     	int result=0;
-//      write your code here
+    	result = x / y;
     	return result;
     }
 
